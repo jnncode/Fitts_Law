9e529e1f00d3a9119b0e2e1e64a1131217ebbd5d
J Nguyen <n.nguyenj@yahoo.com>
2023-03-19 11:52:51 -0500

    Revisions + Addition
    - Format and sizing
    - Created and inputted CSV file to represent DB

--- CircleApplication.py
+++ CircleApplication.py
@@ -1,8 +1,9 @@
 from tkinter import *
 from tkinter import ttk
 import random 
-from time import time 
-
+from time import time
+import tkinter
+import pandas as pd
 
 #class DataHandler():  
     # Create CSV File and add data and then once it is full, navigate to Excel and add file path to open and access 'database'
@@ -14,11 +15,12 @@ from time import time
 class Participants(): 
     def __init__(self):
         self.id = None
-        self.age = None
+        self.age = None # Add condition for partipants to be at least 18 or older 
         self.gender = None
         self.dominance = None # handedness
 
-class Application(Tk):
+# Main Window 
+class Application(Tk): 
     def __init__(self): 
         Tk.__init__(self)
         self._frame = None
@@ -29,7 +31,7 @@ class Application(Tk):
         """Configuration of Application"""
         self.attributes("-fullscreen", False) # Application begins windowed format
         self.resizable(width=False, height=False) # Restrict resizability
-        self.maxsize(800, 800)
+        self.maxsize(1000, 1000)
         self.title("Fitts' Law")
 
     def changePage(self, frame_class, id=None):
@@ -41,50 +43,47 @@ class Application(Tk):
         if self._frame is not None: 
             self._frame.destroy()
         self._frame = new_frame
-        self._frame.pack()
+        self._frame.grid()
 
     
 # Main Page of Application
 class ConsentPage(Frame):
     def __init__(self, master=None):
-        frame = Frame.__init__(self, master)
-       
-        title = StringVar() 
-        labelTitle = Message(self, textvariable=title)
+        Frame.__init__(self, master)
 
-        title.set("Consent Form")
-        labelTitle.config(font=25, justify=CENTER)
-        labelTitle.pack(pady=50)
+        labelConsent = Label(self, text = "The following research application is being conducted by J Nguyen in the CSET Department with the guidance of Dr. Salivia Guario.\n"
+                    "The application is to evaluate and analyze the results following the Law of Fitts. The law is a predictive model of human movement \n"
+                    "primarily used in human-computer interaction and ergonomics. Participants are expected to click the randomly generated circles \n"
+                    "accurately and precisely within 32 trials. The data recorded will be stored into a database and be included in a report based on \n"
+                    "the demographic responses. The completion time will be between 5 to 10 minutes.")
+        
+        labelConsent.config(font=('Raleway', 12))
+        labelConsent.grid(row=0, column=0)
 
-        consent = StringVar() 
-        labelConsent = Message(self, textvariable=consent)
-        consent.set("The following research application is being conducted by J Nguyen in the CSET Department with the guidance of Dr. Salivia Guario.\n" +
-                    "The application is to evaluate and analyze the results following the Law of Fitts. The law is a predictive model of human movement" +
-                    " primarily used in human-computer interaction and ergonomics. Participants are expected to click the randomly generated circles" +
-                    " accurately and precisely within 32 trials. The data recorded will be stored into a database and be included in a report based on" +
-                    " the demographic responses. The completion time will be between 5 to 10 minutes.")
-        labelConsent.config(font=18, relief=SOLID)
-        labelConsent.pack(padx=50, pady=25)
+        self.columnconfigure(0, minsize=1000, weight=6)
+        self.rowconfigure(0, minsize=600, weight=6)
 
         def clickAgree():
             master.changePage(QuestionPage)
 
         mb_agree = Button(self, text="I Agree", relief=RAISED, command=clickAgree) # Add command Agree 
         mb_agree.menu = Menu(mb_agree, tearoff=0)
-        mb_agree.pack()
+        mb_agree.grid(row=1, column=0)
             
         def clickDisagree(): 
             self.quit()
 
         mb_disagree = Button(self, text="I Decline", relief=RAISED, command=clickDisagree)
         mb_disagree.menu = Menu(mb_disagree, tearoff=0)
-        mb_disagree.pack()
+        mb_disagree.grid()
+        self.rowconfigure(2, minsize=100, weight=6)
+
 
 
 # List of questions in one page and must be required fields before proceeding 
 class QuestionPage(Frame):
     def __init__(self, master=None):
-        frame = Frame.__init__(self, master)
+        Frame.__init__(self, master)
 
         question1 = Label(self, text="Age?")
         question1Entry = Entry(self)
@@ -92,6 +91,18 @@ class QuestionPage(Frame):
         question2Entry = Entry(self)
         question3 = Label(self, text="Handedness?")
         question3Radio = Radiobutton(self)
+
+        self.columnconfigure(0, minsize=1000, weight=6)
+        self.rowconfigure(0, minsize=600, weight=6)
+
+        def Submit():
+            master.changePage(InstructionPage)
+
+        mb_submit = Button(self, text="Submit", relief=RAISED, command=Submit)
+        mb_submit.menu = Menu(mb_submit, tearoff=0)
+        mb_submit.grid()
+        self.rowconfigure(2, minsize=100, weight=6)
+
 
     #def requirement():
         
@@ -108,16 +119,15 @@ class QuestionPage(Frame):
 
 class InstructionPage(Frame):
     def __init__(self, master=None):
-        frame = Frame.__init__(self, master)
-        instruction = StringVar() 
-        labelInstruction = Message(self, textvariable=instruction)
-        instruction.set("Instructions\n\n\n" + 
+        Frame.__init__(self, master)
+
+        labelInstruction = Message(self, text = "Instructions\n\n\n" + 
                         "Green circles will randomly generate within the screen." +
                         " Each circle must be clicked in order to proceed, and will be done 32 times." + 
                         " Any misclicks outside of the circles will effect the performance results." +
                         " Progress will be visibly displayed on the top of the screen.")
         labelInstruction.config(font=18, relief=SOLID)
-        labelInstruction.pack(padx=50, pady=25)
+        labelInstruction.grid(padx=50, pady=25)
 
 class Circle: 
     def __init__(self, parent, canvas, id): 
@@ -131,7 +141,7 @@ class CirclePage:
         pass
     def generateCircle(canvas): 
         canvas.circle(110, 10, 210, 110, outline = "BLACK", fill = "GREEN", width = 2)
-        canvas.pack()
+        canvas.grid()
         
         # canvas.bind('<B1-Motion>', handler)
         coordinates = []
@@ -140,17 +150,21 @@ class CirclePage:
 # After partipants completed trials, populate results and close application 
 # Store the data into the database from this current page similarly to when partipants answer the demographic questions to begin trials
 class ResultPage(Frame): 
-    pass
+    def __init__(self, master=None)
+        Frame.__init__(self, master)
+
     # May delete later if not utilized (optional page)
 
 class ThankPage(Frame):
     def __init__(self, master=None): 
-        frame = Frame.__init__(self, master)
-        thankyou = StringVar()
-        labelthankyou = Message(self, textvariable=thankyou)
-        thankyou.set("Thank You!\n" + "All tasks have been completed and data has been collected.")
+        Frame.__init__(self, master)
+        labelthankyou = Label(self, text= "Thank You!\n" + "All tasks have been completed and data has been collected.")
         labelthankyou.config(font=18, relief=SOLID)
-        labelthankyou.pack(padx=50, pady=25)
+        labelthankyou.grid(padx=50, pady=25)
+
+        # Testing - Results will populate for that specific participant and the data collected will be added onto the database (csv file) < loop
+        df = pd.read_csv('Fitts_Data.csv')
+        print(df)
 
 
 if __name__ == "__main__":
