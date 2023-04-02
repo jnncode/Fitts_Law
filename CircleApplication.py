a420b83d9a421ce5282e85aebd5ba4b2bfdcfee7
J Nguyen <n.nguyenj@yahoo.com>
2023-04-02 12:59:24 -0500

    Data Record
    - Record data into CSV file
    - Changes of variable names
    - Removal of unneeded code

--- CircleApplication.py
+++ CircleApplication.py
@@ -1,32 +1,11 @@
 from tkinter import *
-from tkinter import ttk
 from tkinter import font
 from tkinter import messagebox
-import tkinter
+import tkinter as tk
 import time
 import pandas 
 import random  
 import csv
-
-class DataHandler():  
-    with open('Fitts_Data.csv', 'a', newline='') as file:
-        write = csv.writer(file)
-        data = [] # write data 
-        # add condition of when user is done with the questions page, record the answers and proceed to the instructions page 
-        write.writerow(data)
-
-    # Create CSV File and add data and then once it is full, navigate to Excel and add file path to open and access 'database'
-    # When user completes the demographic questions, add results to excel spread
-    # Similarly when they have completed their trials of clicking, add those results to the excel spread 
-
-
-# Included under Questions Page 
-class Participants(): 
-    def __init__(self):
-        self.id = None
-        self.age = None # Add condition for partipants to be at least 18 or older 
-        self.gender = None
-        self.dominance = None # handedness
 
 # Main Window 
 class Application(Tk): 
@@ -60,15 +39,14 @@ class Application(Tk):
 # Main Page of Application
 class ConsentPage(Frame):
     def __init__(self, master=None):
-        assert isinstance(Canvas, tkinter.Canvas)
         Frame.__init__(self, master)
 
-        labelConsent = Label(self, text = "Consent Form\n\n\nThe following research application is being conducted by J Nguyen in the CSET Department with the guidance of Dr. Salivia Guario.\n"
+        label_consent = Label(self, text = "Consent Form\n\n\nThe following research application is being conducted by J Nguyen in the CSET Department with the guidance of Dr. Salivia Guario.\n"
                     "The application is to evaluate and analyze the results following the Law of Fitts. The law is a predictive model of human movement \n"
                     "primarily used in human-computer interaction and ergonomics. Participants are expected to click the randomly generated circles \n"
                     "accurately and precisely within 32 trials. The data recorded will be stored into a database and be included in a report based on \n"
                     "the demographic responses. The completion time will be between 5 to 10 minutes.")
-        labelConsent.grid(row=0, column=0)
+        label_consent.grid(row=0, column=0)
 
         self.columnconfigure(0, minsize=1000, weight=6)
         self.rowconfigure(0, minsize=600, weight=6)
@@ -94,18 +72,44 @@ class QuestionPage(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)  # add padding to the frame
 
-        question1 = Label(self, text="Age?").grid(row=1, column=1, sticky="nsew")  
-        question1Entry = Entry(self)
-        question1Entry.grid(row=2, column=1, sticky="nsew")  
+        def generateId():
+            global participant_count
+            participant_count = 0
+            id = f'P{participant_count + 1:04d}'
+            participant_count += 1 
+            return id
+        
+        generate_id = generateId()
+        # messagebox.showinfo('Participant ID: ', f'{id}') # display for user
+        id_label = Label(self, text="ID").grid(row=1, column=1, sticky="nsew")
+        id_entry = Entry(self)
+        # id_entry.insert(0, generate_id)
+        id_entry.grid(row=1, column=2, sticky="nsew") 
+        # addIdEntry()
 
-        question2 = Label(self, text="Gender?").grid(row=3, column=1, sticky="nsew")  
-        question2Entry = Entry(self)
-        question2Entry.grid(row=4, column=1, sticky="nsew") 
+        # def addIdEntry():
+        #     # Get the current text in the entry widget
+        #     text = id_entry.get()
+            
+        #     if text == '':
+        #         # If the entry widget is empty, insert the new data
+        #         id_entry.insert(0, generate_id)
+        #     else:
+        #     # If the entry widget already has some text, replace it with the new data
+        #         id_entry.delete(0, 'end')
+        #         id_entry.insert(0, generate_id)
+        
+        age_label = Label(self, text="Age").grid(row=3, column=1, sticky="nsew")  
+        age_entry = Entry(self)
+        age_entry.grid(row=2, column=1, sticky="nsew")  
 
+        gender = Label(self, text="Gender").grid(row=5, column=1, sticky="nsew")  
+        gender_entry = Entry(self)
+        gender_entry.grid(row=4, column=1, sticky="nsew") 
 
-        question3 = Label(self, text="Handedness?").grid(row=5, column=1, sticky="nsew")  
-        question3Entry = Entry(self)
-        question3Entry.grid(row=6, column=1, sticky="nsew")
+        hand_label = Label(self, text="Handedness").grid(row=7, column=1, sticky="nsew")  
+        hand_entry = Entry(self)
+        hand_entry.grid(row=6, column=1, sticky="nsew")
 
         self.columnconfigure(0, minsize=450, weight=1)
         self.rowconfigure(0, minsize=400, weight=1)
@@ -113,21 +117,22 @@ class QuestionPage(Frame):
         def validate():
             # Check if age is valid
             try:
-                value = int(question1Entry.get())
-                if value < 0 or value > 100:
+                value1 = int(age_entry.get())
+                value2 = int(id_entry.get())
+                if value1 and value2 < 0 or value1 and value2 > 100:
                     raise ValueError
             except ValueError:
                 messagebox.showerror("Error", "Please enter valid age between 0 and 100.")
                 return False
 
             # Check if gender is filled and valid
-            gender = question2Entry.get().strip().casefold()
+            gender = gender_entry.get().strip().casefold()
             if gender not in ["male", "female", "other"]:
                 messagebox.showerror("Error", "Please enter valid gender (male, female, or other).")
                 return False
 
             # Check if handedness is selected
-            handedness = question3Entry.get().strip().casefold()
+            handedness = hand_entry.get().strip().casefold()
             if handedness not in ["left", "right"]:
                 messagebox.showerror("Error", "Please enter valid handedness (left or right).")
                 return False
@@ -135,13 +140,25 @@ class QuestionPage(Frame):
             # If all fields have been filled
             return True
 
-
         def clickSubmit():
+            id = id_entry.get()
+            age = age_entry.get()
+            gender = gender_entry.get()
+            hand = hand_entry.get()
+
+            # Store the data in CSV file (database)
+            with open('Fitts_Data.csv', mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([id, age, gender, hand])
+    
+
             if validate():
+                messagebox.showinfo("Submitted.\nThank You!")
                 master.changePage(InstructionPage)
             else:
                 messagebox.showerror("Error", "Please fill out all required fields before submitting.")
 
+        
         # Disable the submit button by default
         mb_submit = Button(self, text="Submit", command=clickSubmit)
         mb_submit.menu = Menu(mb_submit, tearoff=0)
@@ -157,66 +174,60 @@ class InstructionPage(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
 
-        labelInstruction = Message(self, text = "Instructions\n\n\n" + 
+        label_instruction = Message(self, text = "Instructions\n\n\n" + 
                         "Green circles will randomly generate within the screen." +
                         " Each circle must be clicked in order to proceed, and will be done 32 times." + 
                         " Any misclicks outside of the circles will effect the performance results." +
                         " Progress will be visibly displayed on the top of the screen.")
-        labelInstruction.grid(row=0, column=0)
+        label_instruction.grid(row=0, column=0)
 
         self.columnconfigure(0, minsize=1000, weight=6)
         self.rowconfigure(0, minsize=600, weight=6)
 
         def begin():
-            master.changePage(CirclePage)
+            master.changePage() # CirclePage - Commented due to Error
 
         mb_begin = Button(self, text="Begin", relief=RAISED, command=begin) # Add command Agree 
         mb_begin.menu = Menu(mb_begin, tearoff=0)
         mb_begin.grid(row=1, column=0)
 
-# Colored circle for user to click (32 count)
-class CirclePage(Frame): 
-    def __init__(self, canvas, master=None):
-        Frame.__init__(self, master)
-        self.canvas = canvas
-        self.circleRadius = 20  
-        self.numberOfCircles = 32  
-        self.circles = []
+# Colored circle for user to click (32 count) - ERROR FIX SOON
+# class CirclePage(Frame): 
+#     def __init__(self, canvas, master=None):
+#         Frame.__init__(self, master)
+#         # Check canvas object is tkinter.Canvas object  
+#         self.canvas = tk.Canvas
+#         if not isinstance(self.canvas, tk.Canvas):
+#             raise TypeError("canvas must be a tkinter.Canvas object")
+#         self.circle_radius = 20  
+#         self.number_of_circles = 32  
+#         self.circles = []
 
-        for i in range(self.numberOfCircles):
-            circle_x = random.randint(self.circleRadius, self.canvas.winfo_width() - self.circleRadius)
-            circle_y = random.randint(self.circleRadius, self.canvas.winfo_height() - self.circleRadius)
-            circle = {
-                "x": circle_x,
-                "y": circle_y,
-                "radius": self.circleRadius,
-                "clicks": 0
-            }
-            self.canvas.create_oval(circle_x-self.circleRadius, circle_y-self.circleRadius,
-                                circle_x+self.circleRadius, circle_y+self.circleRadius, fill="blue")
-            self.circles.append(circle)
+#         # Initialize the click counter and the list of click intervals
+#         click_count = 0
+#         click_intervals = []
 
-        # Track clicks
-        def clickCircle(event):
-            for circle in self.circles:
-                if ((event.x - circle["x"])**2 + (event.y - circle["y"])**2) <= circle["radius"]**2:
-                    circle["clicks"] += 1
-                    if circle["clicks"] >= self.numberOfCircles:
-                        self.canvas.delete("all")
-                        self.circles.remove(circle)
-                        for remainingCircle in self.circles:
-                            self.canvas.create_oval(remainingCircle["x"]-remainingCircle["radius"],
-                                                remainingCircle["y"]-remainingCircle["radius"],
-                                                remainingCircle["x"]+remainingCircle["radius"],
-                                                remainingCircle["y"]+remainingCircle["radius"],
-                                                fill="blue")
-                        self.canvas.bind("<Button-1>", clickCircle) 
-                        if len(self.circles) == 0:
-                            self.canvas.create_text(self.canvas.winfo_width()/2, self.canvas.winfo_height()/2, text="Complete")
-                            self.unbind("<Button-1>")
-                            break
-        self.canvas.bind("<Button-1>", clickCircle) 
+#         # Define a function to handle a click on a circle
+#         def handle_click(event):
+#             nonlocal click_count, click_intervals
+#             click_count += 1
+#             click_intervals.append(time.time() - start_time)
+#             canvas.delete(event.widget)
+#             if click_count == self.number_of_circles:
+#                 end_time = time.time()
+#                 print("Total Time: {:.2f} seconds".format(end_time - start_time))
+#                 print("Click Interals:", click_intervals)
+#                 self.destroy()
 
+#         # Generate the circles
+#         for i in range(self.number_of_circles):
+#             x = random.randint(self.circle_radius, self.canvas.winfo_width() - self.circle_radius)
+#             y = random.randint(self.circle_radius, self.canvas.winfo_height() - self.circle_radius)
+#             circle = tk.Canvas.create_oval(x - self.circle_radius, y - self.circle_radius, x + self.circle_radius, y + self.circle_radius, fill="green")
+#             canvas.tag_bind(circle, "<Button-1>", handle_click)
+#             self.circles.append(circle)
+#         # Start the timer
+#         start_time = time.time()
 
 
 # After partipants completed trials, populate results and close application 
@@ -232,9 +243,9 @@ class ResultPage(Frame):
 class ThankPage(Frame):
     def __init__(self, master=None): 
         Frame.__init__(self, master)
-        labelthankyou = Label(self, text= "Thank You!\n" + "All tasks have been completed and data has been collected.")
-        labelthankyou.config(font=18, relief=SOLID)
-        labelthankyou.grid(padx=50, pady=25)
+        label_thank_you = Label(self, text= "Thank You!\n" + "All tasks have been completed and data has been collected.")
+        label_thank_you.config(font=18, relief=SOLID)
+        label_thank_you.grid(padx=50, pady=25)
 
         def Close():
             app.quit()
