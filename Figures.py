9ed49e5bad0f5a6183ea1aedc33dacacfa0ceb65
J Nguyen <n.nguyenj@yahoo.com>
2023-04-15 20:19:07 -0500

    More Data Retrieved
    - Data of increased circle size
    - Graphs

--- Figures.py
+++ Figures.py
@@ -3,12 +3,28 @@ import numpy
 import pandas
 import matplotlib.pyplot as plt
 
-data = pandas.read_csv("Fitts_Data.csv")
+columns = ["ID", "Age", "Gender", "Hand", "Completion Time(s)", "Inaccurate Clicks"]
+data = pandas.read_csv("Fitts_Data.csv", usecols=columns) # Specify index
 print(data)
 
-plt.xlabel("Completion Time")
-x = (data.columns[4])
-plt.ylabel("Age")
-y = (data.columns[1])
+# Scatter Plot - Age
+x = data["Age"][:10]
+y = data["Completion Time(s)"][:10]
+plt.scatter(x, y,c="b")
+plt.xlabel("Age")
+plt.ylabel("Completion Time(s)")
 plt.title("Completion Time Based on Age")
 plt.show()
+
+# Scatter Plot - Gender
+data["Gender"] = data["Gender"].str.lower()
+gender_labels = {"female": "Female", "male": "Male", "other": "Other"}
+data["Gender"] = data["Gender"].replace(gender_labels)
+x = data["Gender"][:10]
+y = data["Completion Time(s)"][:10]
+plt.scatter(x, y, c="b")
+plt.xlabel("Gender")
+plt.ylabel("Completion Time(s)")
+plt.title("Completion Time Based on Gender")
+plt.show()
+
